8a9084d195bfb766613c9dd668e8bbddcfad6a17
lika <malika24@mhs.unitama.ac.id>
2026-04-24 15:09:52 +0800

    feat: penyiapan blade component untuk layout utama

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/massages', function () {
+    return view('massages.index', ['title' => 'Massages']);
+});
+Route::get('/massages/create', function () {
+    return view('massages.create', ['title' => 'Create Massage']);
+});
+
