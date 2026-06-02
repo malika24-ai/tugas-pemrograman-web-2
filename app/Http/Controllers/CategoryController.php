0f3559ec225c8b24f81d192853c28c8d04bed908
lika <malika24@mhs.unitama.ac.id>
2026-06-02 23:41:55 +0800

    : feat: implementasi halaman show (detail) untuk [brend]

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -53,7 +53,10 @@ class CategoryController extends Controller
      */
     public function show(Category $category)
     {
-       
+        return view('category.show', [
+            'title' => 'Detail Category',
+            'category' => $category,
+        ]);
     }
 
     /**
